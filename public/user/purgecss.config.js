337326a6c175b4286d793f47326047092c799faf
Hardik Kanajariya <work24to7@gmail.com>
2023-09-05 20:45:03 +0530

    Initial Commit in user UI Branch
    Signed-off-by: hacki <work24to7@gmail.com>

---
+++ public/user/purgecss.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    content: ['./**/*.html'], // Specify the HTML files to analyze using globs
+    css: ['./style/*.css'], // Specify the CSS files to optimize using globs
+    // Other options, such as extractors or safelist, can be configured here
+};
