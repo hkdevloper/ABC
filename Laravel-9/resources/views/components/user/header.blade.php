3bedf7a4f3350bf6adb0df00dd7f227a3c1e058f
hacki <work24to7@gmail.com>
2023-10-04 05:24:45 +0530

    removed laravel 9 Directory and installed laravel datatable

--- Laravel-9/resources/views/components/user/header.blade.php
+++
@@ -1,6 +0,0 @@
-<div class="flex justify-between items-center py-2 bg-purple-600">
-    <div class="container lg:px-24 md:px-12 mx-auto flex items-center justify-between">
-        <h1 class="text-white text-4xl uppercase ml-2">{{$title}}</h1>
-        <x-user.bread-crumb/>
-    </div>
-</div>
