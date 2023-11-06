da3dfba962b10ef377cdaef70b8c14b27a7f8741
hacki <work24to7@gmail.com>
2023-11-07 04:52:28 +0530

    removed Unwanted Packages

--- app/Filament/Widgets/StatsOverview.php
+++ app/Filament/Widgets/StatsOverview.php
@@ -3,6 +3,13 @@
 namespace App\Filament\Widgets;
 
 use App\Filament\User\Resources\CompanyResource;
+use App\Models\Blog;
+use App\Models\Company;
+use App\Models\Deal;
+use App\Models\Event;
+use App\Models\Job;
+use App\Models\Product;
+use Faker\Core\Color;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 
@@ -10,72 +17,69 @@ class StatsOverview extends BaseWidget
 {
     protected function getStats(): array
     {
-        $company = \App\Models\Company::where('user_id', auth()->user()->id)->orderBy('created_at', 'desc')->first();
         $prefix = '/user/dashboard';
+        $companyUrl = '';
+        $company = Company::where('user_id', auth()->user()->id)->orderBy('created_at', 'desc')->first();
+        if ($company) {
 
-        $companyUrl = "dashboard/companies/$company->id";
-        if(auth()->user()->type == 'Admin'){
+            $companyUrl = "dashboard/companies/$company->id";
+        }
+        if (auth()->user()->type == 'Admin') {
             $companyUrl = "admin/companies";
             $prefix = 'admin';
         }
-
         return [
-            Stat::make('Companies', function (){
-                if(auth()->user()->type == 'Admin'){
-                    return \App\Models\Company::count();
-                }
-                else{
-                    return \App\Models\Company::where('user_id', auth()->user()->id)->count();
+            Stat::make('Companies', function () {
+                if (auth()->user()->type == 'Admin') {
+                    return Company::count();
+                } else {
+                    return Company::where('user_id', auth()->user()->id)->count();
                 }
             })
+                ->color('#363062')
                 ->url($companyUrl)
                 ->icon('heroicon-o-building-office-2'),
-            Stat::make('Products', function (){
-                if(auth()->user()->type == 'Admin'){
-                    return \App\Models\Product::count();
-                }
-                else{
-                    return \App\Models\Product::where('user_id', auth()->user()->id)->count();
+            Stat::make('Products', function () {
+                if (auth()->user()->type == 'Admin') {
+                    return Product::count();
+                } else {
+                    return Product::where('user_id', auth()->user()->id)->count();
                 }
             })
                 ->url("$prefix/products")
                 ->icon('heroicon-o-shopping-cart'),
-            Stat::make('Events', function (){
-                if(auth()->user()->type == 'Admin'){
-                    return \App\Models\Event::count();
-                }
-                else{
-                    return \App\Models\Event::where('user_id', auth()->user()->id)->count();
+            Stat::make('Events', function () {
+                if (auth()->user()->type == 'Admin') {
+                    return Event::count();
+                } else {
+                    return Event::where('user_id', auth()->user()->id)->count();
                 }
             })
                 ->url("$prefix/events")
                 ->icon('heroicon-o-calendar'),
-            Stat::make('Jobs', function (){
-                if(auth()->user()->type == 'Admin'){
-                    return \App\Models\Job::count();
-                }
-                else{
-                    return \App\Models\Job::where('user_id', auth()->user()->id)->count();
+            Stat::make('Jobs', function () {
+                if (auth()->user()->type == 'Admin') {
+                    return Job::count();
+                } else {
+                    return Job::where('user_id', auth()->user()->id)->count();
                 }
             })
                 ->url("$prefix/jobs")
                 ->icon('heroicon-o-briefcase'),
-            Stat::make('Blogs', function (){
-                if(auth()->user()->type == 'Admin'){
-                    return \App\Models\Blog::count();
-                }
-                else{
-                    return \App\Models\Blog::where('user_id', auth()->user()->id)->count();
+            Stat::make('Blogs', function () {
+                if (auth()->user()->type == 'Admin') {
+                    return Blog::count();
+                } else {
+                    return Blog::where('user_id', auth()->user()->id)->count();
                 }
             })
                 ->url("$prefix/blogs")
                 ->icon('heroicon-o-newspaper'),
-            Stat::make('Deals', function (){
-                if(auth()->user()->type == 'Admin'){
-                    return \App\Models\Deal::count();
-                }
-                else{
-                    return \App\Models\Deal::where('user_id', auth()->user()->id)->count();
+            Stat::make('Deals', function () {
+                if (auth()->user()->type == 'Admin') {
+                    return Deal::count();
+                } else {
+                    return Deal::where('user_id', auth()->user()->id)->count();
                 }
             })
                 ->url("$prefix/deals")
