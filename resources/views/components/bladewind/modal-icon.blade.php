f766a2a8e9ef10ad69e8a1c2359d4e694736e73c
hacki <work24to7@gmail.com>
2023-12-08 01:49:50 +0530

    Composer Update

--- resources/views/components/bladewind/modal-icon.blade.php
+++ resources/views/components/bladewind/modal-icon.blade.php
@@ -11,7 +11,11 @@
     $warning_css = ($type !== 'warning') ? 'hidden' : '';
     $class = sprintf( 'h-14 w-14 rounded-full modal-icon %s', $class);
 @endphp
-<x-bladewind::icon name="check-circle" class="success text-green-600 dark:!text-green-600 {{$class}} {{$success_css}}" />
-<x-bladewind::icon name="hand-raised" class="error text-red-600 dark:!text-red-600 {{$class}} {{$error_css}}" />
-<x-bladewind::icon name="exclamation-triangle" class="warning text-amber-600 dark:!text-amber-600 {{$class}} {{$warning_css}}" />
-<x-bladewind::icon name="information-circle" class="info text-blue-600 dark:!text-blue-600 {{$class}} {{$info_css}}" />
+<x-bladewind::icon name="check-circle"
+                   class="success text-green-500 dark:!text-green-600 {{$class}} {{$success_css}}"/>
+<x-bladewind::icon name="hand-raised"
+                   class="error text-red-500 dark:!text-red-600 {{$class}} {{$error_css}}"/>
+<x-bladewind::icon name="exclamation-triangle"
+                   class="warning text-amber-500 dark:!text-amber-500 {{$class}} {{$warning_css}}"/>
+<x-bladewind::icon name="information-circle"
+                   class="info text-blue-600 dark:!text-blue-600 {{$class}} {{$info_css}}"/>
