33713a5b784eb8f7f419101169fb23f9c83bc5b6
hacki <work24to7@gmail.com>
2024-01-03 22:43:03 +0530

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -171,7 +171,7 @@
                             </div>
                             <div class="mb-2 w-[calc(80%-1rem)]">
                                 <a href="{{ route('view.company', [$company->slug]) }}"
-                                   class="text-purple-500 mb-1 bg-purple-100 hover:bg-purple-500 hover:text-white rounded-full p-1 mr-5 transition duration-300 ease-in-out flex items-center justify-center transform hover:-translate-y-1 hover:scale-60 text-center">
+                                   class="text-purple-500 mb-1 bg-purple-100 hover:bg-purple-500 hover:text-white rounded-full p-1 m-6 transition duration-300 ease-in-out flex items-center justify-center transform hover:-translate-y-1 hover:scale-60 text-center">
                                     View Profile &nbsp;
                                     <i class='bx bx-link-external text-2xl mr-2'></i>
                                 </a>
