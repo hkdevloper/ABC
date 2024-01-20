df2406a560ae534f2996a67e1f7ce666e0804989
hacki <work24to7@gmail.com>
2024-01-20 08:59:52 +0530

    Update list.blade.php

--- resources/views/pages/job/list.blade.php
+++ resources/views/pages/job/list.blade.php
@@ -18,8 +18,8 @@
             </select>
         </div>
         {{-- Total Products --}}
-        <div class="">
-            <p>
+        <div class="mx-4 md:mx-0">
+            <p class="text-xs md:text-base">
                 Showing {{ $jobs->firstItem() }} - {{ $jobs->lastItem() }} of {{ $jobs->total() }} results
             </p>
         </div>
@@ -48,7 +48,7 @@
     <div class="container py-6 mx-auto">
         <!-- Existing content remains unchanged -->
         @forelse($jobs as $job)
-            <div class="company-card bg-white rounded-lg shadow-md overflow-hidden transform transition-transform duration-300 ease-in-out hover:-translate-y-2 flex items-center justify-center p-2 mb-5">
+            <div class="hidden md:flex company-card bg-white rounded-lg shadow-md overflow-hidden transform transition-transform duration-300 ease-in-out hover:-translate-y-2 items-center justify-center p-2 mb-5">
                 <div class="mb-4 p-2 pr-3">
                     <img class="w-full h-20 object-contain overflow-hidden" src="{{ url('storage/' . $job->thumbnail) }}"
                          alt="">
@@ -69,6 +69,32 @@
                        class="text-purple-500 bg-purple-100 hover:bg-purple-500 hover:text-white rounded-full p-1 mt-1 transition duration-300 ease-in-out flex items-center justify-center transform hover:-translate-y-1 hover:scale-60 text-center">
                         {{$job->employment_type}} &nbsp;
                     </a>
+                </div>
+            </div>
+            <div class="flex md:hidden company-card bg-white rounded-lg shadow-md overflow-hidden transform transition-transform duration-300 ease-in-out hover:-translate-y-2 p-2 mb-5">
+                <div class="overflow-hidden mb-4 p-2 md:border-r border-r-1 border-solid border-gray-300" style="width: 80px; height: 80px">
+                    <img class="object-contain overflow-hidden"
+                         src="{{ url('storage/' . $job->thumbnail) }}" width="250"
+                         alt="">
+                </div>
+                <div class="flex justify-center items-center flex-col">
+                    <div class="w-full mx-3 ml-5 flex flex-col items-start justify-center" style="border-right: 1px solid lightgray">
+                        <a href="{{ route('view.job', [$job->slug]) }}" class="flex flex-nowrap items-center mb-3">
+                            <span class="text-base mr-3">{{$job->title}}</span>
+                        </a>
+                            <div class="text-sm text-gray-500 mb-3">
+                            <i class='bx bx-been-here text-red-500'></i> {{$job->address->state->name}}, {{$job->address->country->name}}
+                        </div>
+                        <div class="text-purple-600 text-base">
+                            {{$job->organization}}
+                        </div>
+                    </div>
+                    <div class="w-full">
+                        <a href="{{ route('view.job', [$job->slug]) }}"
+                           class="text-purple-500 bg-purple-100 hover:bg-purple-500 hover:text-white rounded-full p-1 mt-1 transition duration-300 ease-in-out flex items-center justify-center transform hover:-translate-y-1 hover:scale-60 text-center">
+                            {{$job->employment_type}} &nbsp;
+                        </a>
+                    </div>
                 </div>
             </div>
         @empty
