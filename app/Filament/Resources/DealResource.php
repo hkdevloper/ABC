c8e7cee99b52233f4aef4e8268855adac63ee112
hacki <work24to7@gmail.com>
2024-02-19 20:05:48 +0530

    Update DealResource.php

--- app/Filament/Resources/DealResource.php
+++ app/Filament/Resources/DealResource.php
@@ -29,7 +29,7 @@ class DealResource extends Resource
 {
     protected static ?string $model = Deal::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+    protected static ?string $navigationIcon = 'heroicon-o-gift-top';
     protected static ?string $navigationGroup = 'Management';
     protected static ?int $navigationSort = 5;
     public static function form(Form $form): Form
