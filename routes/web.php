6c6e9100244492006011a669c6dca6d64849e0b9
hacki <work24to7@gmail.com>
2024-02-19 19:42:47 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -108,7 +108,6 @@ Route::get('/', function () {
     if (count($p) > 8) {
         $products = $products->random(8);
     }
-    return $products;
 
     if (count($c) > 8) {
         $companies = $c->random(8);
@@ -123,6 +122,7 @@ Route::get('/', function () {
     }
 
     $data = compact('products', 'companies', 'events', 'category', 'searchList');
+    return $data;
     return view('welcome')->with($data);
 })->name('home');
 
