136db67e93c38611efa3871d9d61e84f12be38a0
hacki <work24to7@gmail.com>
2024-03-12 22:19:12 +0530

    message update

--- resources/views/filament/hooks/company-profile-alert.blade.php
+++ resources/views/filament/hooks/company-profile-alert.blade.php
@@ -7,6 +7,7 @@
             transition: box-shadow 0.2s ease-in-out;
             display: flex;
             align-items: center;
+            color: white;
             justify-content: center;
             @if(auth()->user()->company->is_approved)
                 display: none;
@@ -31,7 +32,7 @@
 </div>
 
 <!-- Company Rejected -->
-<div style="background: linear-gradient(to bottom left, #f3b441 40%, #eca524 60%);
+<div style="background: linear-gradient(to bottom left, #f34170 40%, #ec2453 60%);
             width: 100%;
             padding: 1rem;
             margin: 1rem 0;
@@ -41,7 +42,8 @@
             display: flex;
             align-items: center;
             justify-content: center;
-            @if(auth()->user()->company->is_rejected)
+            color: white;
+            @if(!auth()->user()->company->is_rejected)
                 display: none;
             @endif
             gap: 0.5rem;"
@@ -49,15 +51,15 @@
      onmouseout="this.style.boxShadow = '0 4px 6px rgba(0, 0, 0, 0.1), 0 1px 3px rgba(0, 0, 0, 0.08)';">
     @svg('heroicon-o-information-circle','w-10 h-10 text-red-500 dark:text-red-300')
 
-    <div style="width: 100%;">
+    <div style="width: 100%; color: white">
         <div style="margin-top: 0.5rem;">
-            <span style="font-size: 1.25rem; font-weight: 600;">Company Rejected</span>
+            <span style="font-size: 1.25rem; font-weight: 600; color: white">Company Rejected</span>
             <br>
             <span style="font-size: 1rem;">
                 Your company profile is rejected. Please contact the administrator for more information.
                 <!-- reason -->
                 <small style="font-size: 0.75rem; display: block;">
-                    Reason: {{ auth()->user()->company->rejection_reason }}
+                    Reason: {{ auth()->user()->company->rejected_reason }}
                 </small>
                 <small style="font-size: 0.75rem; display: block;">
                     It may take 2–7 days to approve your company profile.
