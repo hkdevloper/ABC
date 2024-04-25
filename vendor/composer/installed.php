6a0370ac260df49b89c55b1c156b6fa57044b8c1
root <root@Hkdevs.tech>
2024-04-25 15:44:16 +0000

    Deployed from Github Action

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'laravel/laravel',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '12f5b875ece7865b2305e874277c73791a16fa5f',
+        'reference' => '97847fa831efe51f4ed7457e8d85bc2dd5846796',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -652,7 +652,7 @@
         'laravel/laravel' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '12f5b875ece7865b2305e874277c73791a16fa5f',
+            'reference' => '97847fa831efe51f4ed7457e8d85bc2dd5846796',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
