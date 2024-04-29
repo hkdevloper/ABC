654496bf8d3f6c133d2590405babc7a46961774e
hacki <work24to7@gmail.com>
2024-04-29 13:36:17 +0530

    performed composer update

--- vendor/filament/filament/src/FilamentManager.php
+++ vendor/filament/filament/src/FilamentManager.php
@@ -34,6 +34,8 @@ use Illuminate\Support\Facades\Event;
 
 class FilamentManager
 {
+    protected ?string $currentDomain = null;
+
     protected ?Panel $currentPanel = null;
 
     protected bool $isServing = false;
@@ -528,6 +530,11 @@ class FilamentManager
         return $this->getCurrentPanel()->hasBreadcrumbs();
     }
 
+    public function hasBroadcasting(): bool
+    {
+        return $this->getCurrentPanel()->hasBroadcasting();
+    }
+
     public function hasCollapsibleNavigationGroups(): bool
     {
         return $this->getCurrentPanel()->hasCollapsibleNavigationGroups();
@@ -669,6 +676,11 @@ class FilamentManager
     public function serving(Closure $callback): void
     {
         Event::listen(ServingFilament::class, $callback);
+    }
+
+    public function currentDomain(?string $domain): void
+    {
+        $this->currentDomain = $domain;
     }
 
     public function setCurrentPanel(?Panel $panel): void
@@ -846,5 +858,22 @@ class FilamentManager
     public function arePasswordsRevealable(): bool
     {
         return $this->getCurrentPanel()->arePasswordsRevealable();
+    }
+
+    public function getCurrentDomain(?string $testingDomain = null): string
+    {
+        if (filled($this->currentDomain)) {
+            return $this->currentDomain;
+        }
+
+        if (app()->runningUnitTests()) {
+            return $testingDomain;
+        }
+
+        if (app()->runningInConsole()) {
+            throw new Exception('The current domain is not set, but multiple domains are registered for the panel. Please use [Filament::currentDomain(\'example.com\')] to set the current domain to ensure that panel URLs are generated correctly.');
+        }
+
+        return request()->getHost();
     }
 }
